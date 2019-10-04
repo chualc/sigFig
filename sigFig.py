06f55dea67860c2e16b786ab09bbc761eb39b006
chualc <32863283+chualc@users.noreply.github.com>
2019-10-04 12:34:46 +0800

    Remove decimal number if it is 0

--- sigFig.py
+++ sigFig.py
@@ -1,9 +1,20 @@
 import math
 
+#https://github.com/chualc/sigFig
 def round_sfig(num, sfig):
     if num == 0:
         return 0
     else:
         # https://en.wikipedia.org/wiki/Significant_figures
         r_loc = int(math.floor(math.log10(abs(num))) + 1 - sfig)
-        return round(num, -r_loc)
+        answer = round(num, -r_loc)
+        
+        # EX: 
+        # if answer is 312.0 and int(answer) is 312, return int(answer)
+        # if answer is 31.2 and int(answer) is 31, return answer
+        
+        if answer == int(answer):
+            return int(answer)
+        
+        return answer
+
